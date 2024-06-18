764edf2b87d1af8d924c0ea0dc5ea0a07e3f3629
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2024-06-18 09:12:28 -0400

    Implement endpoint to fetch first record from Greetings table (#6)
    - Set up DatabaseContext for database connections.
    - Developed GreetingRepository for greeting retrieval.
    - Added endpoint to display the first greeting using GreetingRepository.
    - Added .idea to .gitignore

---
+++ src/webapp/Data/DatabaseContext.cs
@@ -0,0 +1,12 @@
+﻿namespace webapp.Data;
+
+public class DatabaseContext
+{
+    public MySqlConnection Connection { get; }
+
+    public DatabaseContext(AppSettings appSettings)
+    {
+        Connection = new MySqlConnection(appSettings.Database.ConnectionString);
+        Connection.Open();
+    }
+}
--- src/webapp/Program.cs
+++ src/webapp/Program.cs
@@ -1,12 +1,24 @@
+var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+var settingsFile = environment == "Production" ? "appsettings.json" : "appsettings.Development.json";
+
 var config = new ConfigurationBuilder()
     .SetBasePath(Directory.GetCurrentDirectory())
-    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile(settingsFile, optional: false, reloadOnChange: true)
     .AddEnvironmentVariables()
     .Build();
 
+var appSettings = config.Get<AppSettings>();
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<DatabaseContext>(_ => new DatabaseContext(appSettings!));
+builder.Services.AddScoped<GreetingRepository>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => $"Hello World! {config.GetValue<string>("PROVIDER")}");
+app.MapGet("/greeting", (GreetingRepository greetingRepository) => greetingRepository.GetGreeting());
+
+app.MapGet("/", () => $"Hello World!");
 
 app.Run();
