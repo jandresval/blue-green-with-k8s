f31a79a1b51a2bee62162a82dc71781725724858
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2024-06-23 17:28:58 -0400

    Improve data retrieval error handling

--- src/frontend/src/views/Landing.tsx
+++ src/frontend/src/views/Landing.tsx
@@ -1,20 +1,22 @@
 import { useState, useEffect } from "react";
 
 function Landing() {
-
     const [data, setData] = useState(String);
     
     useEffect(() => {
         fetch(import.meta.env.VITE_API_URL)
-          .then(response => response.text())
-          .then(text => setData(text))
-          .catch(error => console.error(error));
+          .then((response) => response.text())
+          .then((text) => setData(text))
+          .catch((error) => {
+            console.error(error);
+            setData("Error fetching data");
+          });
       }, []);
 
     return (
         <div className="flex justify-center bg-gray-100 h-screen items-center">
             <div className="text-gray-800 text-center bg-gray-300 px-4 py-2 m-2">
-                {data ? <pre>"{data}"</pre> : 'Loading...'}
+                {data ? <pre>{data}</pre> : 'Loading...'}
             </div>
         </div>
     );
