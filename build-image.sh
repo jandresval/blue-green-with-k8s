26f43b6f931acea8fa2bb1f2e2f48956ee1f1940
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2024-07-05 14:28:06 -0400

    Add Kubernetes deployment configuration to deploy & expose API
    - Create Kubernetes deployment and service configurations for the API
    - Configure NodePort service to expose the API externally
    - Include environment variable settings for seamless integration with the existing database setup
    - Revise README with new app run instructions

---
+++ build-image.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Define variables
+IMAGE_NAME="simple-webapp"
+TAG="latest"
+
+# Uncomment and set the following if using a local registry
+# REGISTRY_URL="your-local-registry"
+
+# Build the Docker image
+docker build -t ${IMAGE_NAME}:${TAG} . --no-cache
+
+# Tag the image for a local registry (optional)
+# docker tag ${IMAGE_NAME}:${TAG} ${REGISTRY_URL}/${IMAGE_NAME}:${TAG}
+
+# Push the image to the local registry (optional)
+# docker push ${REGISTRY_URL}/${IMAGE_NAME}:${TAG}
+
+echo "Build and tag completed."
+
+read -rsp $'\nScript end.'
