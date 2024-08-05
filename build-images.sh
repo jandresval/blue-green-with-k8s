f7e1456db8183d0ef2bfadc7a56c19acd46c79fb
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2024-08-05 10:49:30 -0400

    Refactor build-images script

--- build-images.sh
+++ build-images.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 
-# Define variables
+SKIP_PAUSE="$1" # First Script Argument
+CUSTOM_TAG="$2" # Second Script Argument
+
 BE_IMAGE_NAME="backend"
 FE_IMAGE_NAME="frontend"
-
 TAG="latest"
+
+if [[ -n "$CUSTOM_TAG" ]]; then
+    TAG="$CUSTOM_TAG"
+fi
 
 # Uncomment and set the following if using a local registry
 # REGISTRY_URL="your-local-registry"
 
-# Build the Docker image
-if ! [[ $2 = "" ]]; then
-TAG=$2
-fi
+docker build -t "${BE_IMAGE_NAME}:${TAG}" -f ./src/webapp/Dockerfile ./src/webapp --no-cache
+docker build -t "${FE_IMAGE_NAME}:${TAG}" -f ./src/frontend/Dockerfile ./src/frontend --no-cache
 
-docker build -t ${BE_IMAGE_NAME}:${TAG} . --no-cache
-
-docker build -t ${FE_IMAGE_NAME}:${TAG} -f ./src/frontend/Dockerfile ./src/frontend --no-cache
-
-# Tag the image for a local registry (optional)
-# docker tag ${IMAGE_NAME}:${TAG} ${REGISTRY_URL}/${IMAGE_NAME}:${TAG}
-
-# Push the image to the local registry (optional)
-# docker push ${REGISTRY_URL}/${IMAGE_NAME}:${TAG}
+# Optional: Tag and push the image to a local registry
+# docker tag "${BE_IMAGE_NAME}:${TAG}" "${REGISTRY_URL}/${BE_IMAGE_NAME}:${TAG}"
+# docker tag "${FE_IMAGE_NAME}:${TAG}" "${REGISTRY_URL}/${FE_IMAGE_NAME}:${TAG}"
+# docker push "${REGISTRY_URL}/${BE_IMAGE_NAME}:${TAG}"
+# docker push "${REGISTRY_URL}/${FE_IMAGE_NAME}:${TAG}"
 
 echo "Build and tag completed."
 
-if [[ "$1" = false ]] | [[ "$1" = "" ]]; then
-    read -rsp $'\nScript end.'
+if [[ "$SKIP_PAUSE" != "true" ]]; then
+    read -rsp $'\nScript ended. Press any key to exit...'
 fi
