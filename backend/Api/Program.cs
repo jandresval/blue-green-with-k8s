8183ffc9269936d525708c4a379110c84d202722
Jaime Valencia <valencia@ocas.ca>
2024-08-24 20:54:30 -0400

    Change Greeting repository and Database context

--- backend/Api/Program.cs
+++ backend/Api/Program.cs
@@ -28,6 +28,7 @@ builder.Services.AddCors(options =>
 );
 
 builder.Services.AddSingleton<DatabaseContext>(_ => new DatabaseContext(appSettings!));
+
 builder.Services.AddScoped<GreetingRepository>();
 
 var app = builder.Build();
